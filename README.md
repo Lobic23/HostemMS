2066a3ffef67f5807b0ed5f845c5aa8998b7be09
Rasaili-rain <saditrasaili063@gmail.com>
2026-02-18 18:03:53 +0545

    cleaan

--- README.md
+++ README.md
@@ -1,26 +1,15 @@
-
-
 the bruno/ folder is just a api collection
 https://www.usebruno.com/
 
-
 bruno is just a open source alternative to postman that i really like
 
-to open the collection 
+to open the collection
 start up Bruno, click ' + ' and click open collection
 then choose bruno/hostelMS/
 
 and woohooo we are in
 
-
 PRO tip : bruno also has a vscode extension for easier use
-
-
 
 i am yet to make frontend/
 as i have not intalled flutter yet
-
-
-
-
-
--- backend/src/common/types/api.ts
+++ backend/src/common/types/api.ts
@@ -1,14 +1,16 @@
 export interface ApiResponse<T = void> {
-  status: 'success' | 'error';
+  status: "success" | "error";
   data?: T;
   message?: string;
 }
 
 // helpers
 // just to remove some duplicate code
-export const ok = <T>(data: T): ApiResponse<T> => ({ status: 'success', data });
-export const created = <T>(data: T): ApiResponse<T> => ({ status: 'success', data });
-export const err = (message: string): ApiResponse => ({ status: 'error', message });
+export const ok = <T>(data: T): ApiResponse<T> => ({ status: "success", data });
+export const created = <T>(data: T): ApiResponse<T> => ({
+  status: "success",
+  data,
+});
 
 // USAGE :
 //  getUserById: asyncHandler(async (req: Request, res: Response) => {
